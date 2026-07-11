22edba3d4e3f58eb20752e3426b94b59ed5d6599
ItsFahad-coder <mfahadbhutto@gmail.com>
2026-07-11 17:17:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(__dirname));
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
