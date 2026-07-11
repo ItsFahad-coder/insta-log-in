c6bfb3735ff4631f6a38b661135d888ddf49e3c2
ItsFahad-coder <mfahadbhutto@gmail.com>
2026-07-11 17:42:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Same folder se files serve karega jahan server.js hai
 app.use(express.static(__dirname));
 
-app.post("/login", (req, res) => {
+app.get("/login", (req, res) => {
   const { username, password } = req.body;
 
-  console.log(username, password);
-
-  res.send("Received");
+  res.send(`
+    <html>
+      <head><title>Gallery</title></head>
+      <body style="background:#111; padding:20px;">
+        <h1 style="color:#fff;">${(username, password)}!</h1>
+      </body>
+    </html>
+  `);
 });
 
 const PORT = process.env.PORT || 3000;
@@ -21,3 +27,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
